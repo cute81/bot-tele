0e730d227890117edc6b5b8247814088e9e6a6a8
cute81 <52591440+cute81@users.noreply.github.com>
2022-03-16 13:03:45 +0700

    Update anti_decompile.py

--- anti_decompile.py
+++ anti_decompile.py
@@ -10,21 +10,17 @@ if os.name=='nt':
 else: print('\r\x1b[0;1;92m', end='\r');
 
 class ClAss:
-
     def __init__(self):
         self.ReturN;
-
     def DaTez_Kun(self, teks):
         for o in teks+'\n':
             print(o, end='', flush=True);
             time.sleep(.1);
-
     @property
     def ReturN(self):
         for _ in range(9, -1, -1):
             print(f"\r [*] Wait a Second! {_} ", end='', flush=True);
             time.sleep(.95);
         self.DaTez_Kun('\n [*] Thanks For Waiting');
-
 if __name__ == '__main__':
-    ClAss();
+    ClAsss();
